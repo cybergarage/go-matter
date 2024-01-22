6dafb13a9db2aaf7e8da6849e542c7bee7431da7
Satoshi Konno <skonno@cybergarage.org>
2024-01-22 12:57:55 +0900

    Update protocol.Exchange

--- matter/protocol/exchange.go
+++ matter/protocol/exchange.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package message
+package protocol
 
 // ExchangeFlag represents a exchange flag.
 type ExchangeFlag uint8
