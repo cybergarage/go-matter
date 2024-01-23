e68fd0d4697b06a5464a2ca4f1cd10d2425d609b
Satoshi Konno <skonno@cybergarage.org>
2024-01-23 20:37:10 +0900

    Add Uint*ToBytes() and BytesToUint*()

--- matter/encoding/binary.go
+++ matter/encoding/binary.go
@@ -14,8 +14,8 @@
 
 package encoding
 
-// IntegerToByte converts a specified integer to bytes.
-func IntegerToByte(v uint, b []byte) {
+// IntegerToBytes converts a specified integer to bytes.
+func IntegerToBytes(v uint, b []byte) {
 	byteSize := len(b)
 	for n := 0; n < byteSize; n++ {
 		idx := ((byteSize - 1) - n)
@@ -23,8 +23,8 @@ func IntegerToByte(v uint, b []byte) {
 	}
 }
 
-// ByteToInteger converts specified bytes to a integer.
-func ByteToInteger(b []byte) uint {
+// BytesToInteger converts specified bytes to a integer.
+func BytesToInteger(b []byte) uint {
 	var v uint
 	byteSize := len(b)
 	for n := 0; n < byteSize; n++ {
@@ -32,4 +32,47 @@ func ByteToInteger(b []byte) uint {
 		v += (uint(b[idx]) << (uint(n) * 8))
 	}
 	return v
+}
+
+// Uint8ToBytes converts a specified integer to bytes.
+func Uint8ToBytes(v uint8, b [1]byte) {
+	b[0] = byte(v)
+}
+
+// BytesToUint8 converts specified bytes to a integer.
+func BytesToUint8(b [1]byte) uint8 {
+	return uint8(b[0])
+}
+
+// Uint16ToBytes converts a specified integer to bytes.
+func Uint16ToBytes(v uint16, b [2]byte) {
+	b[0] = byte(v >> 8)
+	b[1] = byte(v & 0xff)
+}
+
+// Byte2ToUint16 converts specified bytes to a integer.
+func Byte2ToUint16(b [2]byte) uint16 {
+	return uint16(b[0])<<8 | uint16(b[1])
+}
+
+// Uint32ToBytes converts a specified integer to bytes.
+func Uint32ToBytes(v uint32, b [4]byte) {
+	b[0] = byte(v >> 24)
+	b[1] = byte(v >> 16)
+	b[2] = byte(v >> 8)
+	b[3] = byte(v & 0xff)
+}
+
+// BytesToUint32 converts specified bytes to a integer.
+func BytesToUint32(b [4]byte) uint32 {
+	return uint32(b[0])<<24 | uint32(b[1])<<16 | uint32(b[2])<<8 | uint32(b[3])
+}
+
+// Uint64ToBytes converts a specified integer to bytes.
+func Uint64ToBytes(v uint64, b [8]byte) {
+}
+
+// BytesToUint64 converts specified bytes to a integer.
+func BytesToUint64(b [8]byte) uint64 {
+	return 0
 }
