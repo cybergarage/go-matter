ce15a564ca5cebc7a32c106c681fb8b22e59d708
Satoshi Konno <skonno@cybergarage.org>
2025-09-10 22:19:01 +0900

    refactor(cli): update matterctl

--- matter/cmd/cli/scan.go
+++ matter/cmd/cli/scan.go
@@ -42,7 +42,7 @@ var scanCmd = &cobra.Command{ // nolint:exhaustruct
 			return err
 		}
 		scanner := SharedCommissioner()
-		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 		defer cancel()
 		err = scanner.Scan(ctx)
 		if err != nil {
