7fa2839e7c0a498eb4a89155649556e51a211025
Satoshi Konno <skonno@cybergarage.org>
2026-02-24 18:44:39 +0900

    feat(types): add SessionType stringer and random SessionID generators

--- matter/types/session.go
+++ matter/types/session.go
@@ -14,6 +14,10 @@
 
 package types
 
+import (
+	"github.com/cybergarage/go-matter/matter/crypto"
+)
+
 const (
 	// UnicastSession indicates a unicast session.
 	UnicastSession SessionType = 0x00
@@ -25,7 +29,36 @@ const (
 // 4.4.1.3. Security Flags (8 bits).
 type SessionType uint8
 
+// String returns a human-readable string representation of the SessionType.
+func (s SessionType) String() string {
+	switch s {
+	case UnicastSession:
+		return "UnicastSession"
+	case GroupSession:
+		return "GroupSession"
+	default:
+		return ""
+	}
+}
+
 // SessionID represents a session identifier.
 // 4.14.1.1. Protocol Overview.
 // 4.13.2.4. Choosing Secure Unicast Session Identifiers.
 type SessionID uint16
+
+// NewSessionID generates a new random session ID for secure unicast sessions.
+func NewSessionID() SessionID {
+	b := crypto.CryptoDRBG(2)
+	val := uint32(b[0])<<8 | uint32(b[1])
+	return SessionID(val)
+}
+
+// NewSessionIDExcept generates a new random session ID that is different from the given session ID.
+func NewSessionIDExcept(id SessionID) SessionID {
+	for {
+		newID := NewSessionID()
+		if newID != id {
+			return newID
+		}
+	}
+}
---
+++ matter/types/session_test.go
@@ -0,0 +1,44 @@
+// Copyright (C) 2025 The go-matter Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package types
+
+import (
+	"testing"
+)
+
+func TestSessionID(t *testing.T) {
+	// Test that NewSessionID returns different values on multiple calls (statistically likely)
+	id1 := NewSessionID()
+	id2 := NewSessionID()
+	if id1 == id2 {
+		t.Logf("Warning: NewSessionID generated duplicate IDs: %v", id1)
+	}
+
+	// Test that NewSessionIDExcept returns a different ID than the given one
+	for i := 0; i < 10; i++ {
+		orig := NewSessionID()
+		newID := NewSessionIDExcept(orig)
+		if newID == orig {
+			t.Errorf("NewSessionIDExcept returned the same ID: %v", orig)
+		}
+	}
+
+	// Test that NewSessionIDExcept returns a valid SessionID
+	orig := SessionID(0x1234)
+	newID := NewSessionIDExcept(orig)
+	if newID == orig {
+		t.Errorf("NewSessionIDExcept returned the same ID as input: %v", orig)
+	}
+}
