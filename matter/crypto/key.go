210717fe6f8db1847032a4946aaa4e62fe113848
Satoshi Konno <skonno@cybergarage.org>
2026-02-25 23:02:23 +0900

    feat(crypto): add ECC key pair generation with interface, implementation, and tests

---
+++ matter/crypto/key.go
@@ -0,0 +1,33 @@
+// Copyright (C) 2025 The go-matter Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package crypto
+
+import "math/big"
+
+// PublicKey represents a public key used in cryptographic operations.
+type PublicKey interface {
+	X() *big.Int
+	Y() *big.Int
+}
+
+// PrivateKey represents a private key used in cryptographic operations.
+type PrivateKey *big.Int
+
+// KeyPair represents a pair of public and private keys used in cryptographic operations.
+// 3.5. Public Key Cryptography.
+type KeyPair interface {
+	Public() PublicKey
+	Private() PrivateKey
+}
---
+++ matter/crypto/key_test.go
@@ -0,0 +1,32 @@
+// Copyright (C) 2025 The go-matter Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package crypto
+
+import (
+	"testing"
+)
+
+func TestCryptoGenerateKeypair(t *testing.T) {
+	kp, err := CryptoGenerateKeypair()
+	if err != nil {
+		t.Fatalf("CryptoGenerateKeypair returned error: %v", err)
+	}
+	if kp.Public() == nil {
+		t.Error("Expected non-nil PublicKey")
+	}
+	if kp.Private() == nil {
+		t.Error("Expected non-nil PrivateKey")
+	}
+}
